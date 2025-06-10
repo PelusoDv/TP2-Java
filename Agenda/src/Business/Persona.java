911d5625aab6b9fba0adbe1b9a120e0dfee8f5b4
Nicolás Alfredo Dávalos <nico.vithe15@gmail.com>
2025-06-10 17:02:47 -0300

    Ordenando un poco la clase Persona

--- Agenda/src/Business/Persona.java
+++ Agenda/src/Business/Persona.java
@@ -4,45 +4,29 @@ package Business;
 public class Persona {
     private String nombre;
     private String apellido;
-    private int dni;
-    private String direc;
-    private int telef;
-    private String nac;
+    private String dni;
+    private String direccion;
+    private String telefono;
+    private String fechaNacimiento;
  
     public Persona() {
     }
     
-    public Persona(String nombre, String apellido, int dni, String direc, int telef, String nac) {
-        this.nombre = nombre;
-        this.apellido = apellido;
+    public Persona(String name, String sur, String dni, String dir, String tel, String nac) {
+        this.nombre = name;
+        this.apellido = sur;
         this.dni = dni;
-        this.direc = direc;
-        this.telef = telef;
-        this.nac = nac;
+        this.direccion = dir;
+        this.telefono = tel;
+        this.fechaNacimiento = nac;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public int getDni() {
-        return dni;
-    }
-
-    public String getDirec() {
-        return direc;
-    }
-
-    public int getTelef() {
-        return telef;
-    }
-
-    public String getNac() {
-        return nac;
-    }
+    // Getters
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+    public String getDni() { return dni; }
+    public String getDireccion() { return direccion; }
+    public String getTelefono() { return telefono; }
+    public String getFechaNacimiento() { return fechaNacimiento; }
     
 }
